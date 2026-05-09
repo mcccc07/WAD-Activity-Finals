5834d2c367e84bd167c8e18f15d618e64727cdae
maki-sushi-distro <macrobertsavandal@gmail.com>
2026-05-09 21:28:03 +0800

    Restore original vite and postcss config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from 'tailwindcss';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
