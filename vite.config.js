c8cb432877643e87c9747e128982835eb1a72e5c
maki-sushi-distro <macrobertsavandal@gmail.com>
2026-05-09 21:06:11 +0800

    Fix Tailwind v4 config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from 'tailwindcss';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
