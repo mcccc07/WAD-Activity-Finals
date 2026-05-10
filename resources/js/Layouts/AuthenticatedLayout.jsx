7e9f73c7a1d3dffd3a16a298aa047dfd7d58e6a1
maki-sushi-distro <macrobertsavandal@gmail.com>
2026-05-10 13:59:46 +0800

    My Order icon Updated

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -39,6 +39,14 @@ export default function AuthenticatedLayout({ header, children }) {
             <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
             </svg>
+        ),
+        orders: (
+            <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M18 4v16a2 2 0 01-2 2H10L5 17V4a2 2 0 012-2h9a2 2 0 012 2z" />
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 17h5v5" />
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2.5} d="M9 8h.01M9 12h.01M9 16h.01" />
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8h3M12 12h3M12 16h3" />
+            </svg>
         )
     };
 
@@ -111,7 +119,7 @@ export default function AuthenticatedLayout({ header, children }) {
                             <NavItem href={route('dashboard')} active={route().current('dashboard')} icon={icons.dashboard} collapsed={collapsed}>
                                 Dashboard
                             </NavItem>
-                            <NavItem href={route('orders.index')} active={route().current('orders.*')} icon={icons.dashboard} collapsed={collapsed} badge={unnotifiedOrdersCount}>
+                            <NavItem href={route('orders.index')} active={route().current('orders.*')} icon={icons.orders} collapsed={collapsed} badge={unnotifiedOrdersCount}>
                                 My Orders
                             </NavItem>
                             <Link
